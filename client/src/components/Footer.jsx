5e9836d321a160d885cd7a2b9aa98f7e82ed1a03
Brayan Markov <brayan_markov@yahoo.com>
2024-07-25 18:03:44 +0300

    Add error page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,21 @@
-
+import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <ul className="footer__categories">
+        <li><Link to="/posts/categories/Agriculture">Agriculture</Link></li>
+        <li><Link to="/posts/categories/Business">Business</Link></li>
+        <li><Link to="/posts/categories/Education">Education</Link></li>
+        <li><Link to="/posts/categories/Entertainment">Entertainment</Link></li>
+        <li><Link to="/posts/categories/Art">Art</Link></li>
+        <li><Link to="/posts/categories/Uncategorized">Uncategorized</Link></li>
+        <li><Link to="/posts/categories/Weather">Weather</Link></li>
+      </ul>
+      <div className="footer__copyright">
+        <small>All Rights Reserved &copy; Copyright, Brayan Markov</small>
+      </div>
+    </footer>
   )
 }
 
--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,10 +1,13 @@
-
+import { Link } from "react-router-dom"
 
 const ErrorPage = () => {
   return (
-    <div>
-      ErrorPage
-    </div>
+    <section className="error-page">
+      <div className="center">
+        <Link to="/" className="btn-primary">Go Back Home</Link>
+        <h2>Page Not Found</h2>
+      </div>
+    </section>
   )
 }
 
