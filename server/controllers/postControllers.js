7cb28b80741aeb19ce0f3ac958229df4613abb12
Brayan Markov <brayan_markov@yahoo.com>
2024-07-27 20:09:55 +0300

    Add resources

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -1,11 +1,60 @@
-
+const Post = require('../models/postModel')
+const User = require('../models/userModel')
+const fs = require('fs')
+const path = require('path')
+const{v4: uuid} = require('uuid')
+const HttpError = require('../models/errorModel')
 
 const createPost = async (req, res, next) => {
-    res.json('Create post')
+    try {
+        let{title, category, description } = req.body;
+
+        if (!title || !category || !description || !req.files) {
+            return next(new HttpError('Missing required fields', 422))
+        }
+
+        const {thumbnail} = req.files;
+        // check the file size
+
+        if (thumbnail.size > 2000000) {
+            return next(new HttpError('Thumbnail file size exceeds 2MB', 422))
+        }
+
+        let fileName = thumbnail.name
+        let splittedFileName = fileName.split('.')
+        let newFileName = splittedFileName[0] + uuid() + '.' + splittedFileName[splittedFileName.length - 1]
+
+        thumbnail.mv(path.join(__dirname, '..', '/uploads', newFileName), async (err) => {
+            if (err) {
+                return next(new HttpError(err))
+            } else {
+                const newPost = await Post.create({title, category, description, thumbnail: newFileName, creator: req.user.id})
+
+                if (!newPost) {
+                    return next(new HttpError('Failed to create post', 422))
+                }
+
+                // find user and increase post count
+                const currentUser = await User.findById(req.user.id)
+                const userPostCount = currentUser.posts + 1
+
+                await User.findByIdAndUpdate(req.user.id, {posts: userPostCount})
+
+                res.status(201).json(newPost)
+            }
+        })
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 const getPosts = async (req, res, next) => {
-    res.json('Get all posts')
+    try {
+        const posts = await Post.find().sort({updatedAt: -1})
+        res.status(200).json(posts)
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 const getPost = async (req, res, next) => {
--- server/models/postModel.js
+++ server/models/postModel.js
@@ -5,6 +5,7 @@ const postSchema = new Schema({
     category: {type: String, enum: ['Agriculture', 'Business', 'Education', 'Entertainment', 'Art', 'Investment', 'Uncategorized', 'Weather'], message: 'Value is not supported'},
     description: {type: String, required: true},
     creator: {type: Schema.Types.ObjectId, ref: 'User'},
+    thumbnail: {type: String, required: true},
 }, {timestamps: true})
 
 module.exports = model('Post', postSchema);
