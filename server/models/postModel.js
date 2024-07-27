fd536489d0f84dd59f28cb1464c1d1b1b4074089
Brayan Markov <brayan_markov@yahoo.com>
2024-07-27 16:38:02 +0300

    Add post model

---
+++ server/models/postModel.js
@@ -0,0 +1,10 @@
+const {Schema, model} = require('mongoose');
+
+const postSchema = new Schema({
+    title: {type: String, required: true},
+    category: {type: String, enum: ['Agriculture', 'Business', 'Education', 'Entertainment', 'Art', 'Investment', 'Uncategorized', 'Weather'], message: 'Value is not supported'},
+    description: {type: String, required: true},
+    creator: {type: Schema.Types.ObjectId, ref: 'User'},
+}, {timestamps: true})
+
+module.exports = model('Post', postSchema);
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,15 +1,15 @@
 const {Router} = require('express');
 
-
-const { createPost, getPosts, getPost, getCategoryPosts, getUserPosts, editPost, deletePost } = require('../controllers/postControllers')
+const { createPost, getPosts, getPost, getCategoryPosts, getUserPosts, editPost, deletePost } = require('../controllers/postControllers');
+const authMiddleware = require('../middleware/authMiddleware');
 const router = Router();
 
-router.post('/', createPost)
+router.post('/', authMiddleware, createPost)
 router.get('/', getPosts)
 router.get('/:id', getPost)
 router.get('/categories/:category', getCategoryPosts)
 router.get('/users/:id', getUserPosts)
-router.patch('/:id', editPost)
-router.delete('/:id', deletePost)
+router.patch('/:id', authMiddleware, editPost)
+router.delete('/:id', authMiddleware, deletePost)
 
 module.exports = router;
